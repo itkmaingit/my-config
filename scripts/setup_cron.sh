889d9c014227ef0d44f4fbd92776a3b3c3d951cd
dango <itkmain.git@gmail.com>
2024-07-26 11:10:40 +0900

    lazy commit

--- scripts/setup_cron.sh
+++ scripts/setup_cron.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ジョブ内容
-JOB="*/30 * * * * ~/.config/unq/scripts/synchronization.sh"
+JOB="*/10 * * * * ~/.config/unq/scripts/synchronization.sh"
 
 # crontab に現在のユーザーのジョブを出力し、新しいジョブを追加
 (crontab -l 2>/dev/null; echo "$JOB") | crontab -
