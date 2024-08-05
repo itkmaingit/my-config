e4d68c81098f62823e3be54d7ec423f72aed622e
Dango <68278041+itkmaingit@users.noreply.github.com>
2024-08-05 18:12:40 +0900

    Create setup_git.sh

---
+++ scripts/setup_git.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+### working at home directory
+cd ~
+
+# Install GitHub CLI and login GitHub
+sudo apt update \
+&& sudo apt upgrade -y \
+&& sudo apt install wget  gcc cron curl libsqlite3-dev libpq-dev libmysqlclient-dev build-essential -y \
+&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt install gh -y \
+&& export BROWSER="/mnt/c/Program Files/Google/Chrome/Application/chrome.exe" \
+&& echo $BROWSER
