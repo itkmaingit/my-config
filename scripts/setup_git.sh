1c38ba2fc19b7f7282d61151a04e77c108cdf246
Dango <68278041+itkmaingit@users.noreply.github.com>
2024-08-05 19:08:27 +0900

    Update setup_git.sh

--- scripts/setup_git.sh
+++ scripts/setup_git.sh
@@ -11,6 +11,4 @@ sudo apt update \
 && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
 && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-&& sudo apt install gh -y \
-&& export BROWSER="/mnt/c/Program Files/Google/Chrome/Application/chrome.exe" \
-&& echo $BROWSER
+&& sudo apt install gh -y
