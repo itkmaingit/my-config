ddc24c6f82307c9809935dae88bc79ca2ec37468
Dango <68278041+itkmaingit@users.noreply.github.com>
2024-08-14 00:11:49 +0900

    Update setup_wsl.sh

--- scripts/setup_wsl.sh
+++ scripts/setup_wsl.sh
@@ -72,3 +72,6 @@ sudo apt autoremove -y
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt install yarn -y
+
+#### thefuck
+pip3 install thefuck --user
