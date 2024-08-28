28e65032e85aacad27d35a353d808b8de335fc7c
Dango <68278041+itkmaingit@users.noreply.github.com>
2024-08-28 11:21:31 +0900

    Update setup_git.sh

--- scripts/setup_git.sh
+++ scripts/setup_git.sh
@@ -6,7 +6,7 @@ cd ~
 # Install GitHub CLI and login GitHub
 sudo apt update \
 && sudo apt upgrade -y \
-&& sudo apt install -y wget  gcc cron curl libsqlite3-dev libpq-dev libmysqlclient-dev build-essential python-is-python3 nodejs npm python3-dev python3-pip python3-setuptools \
+&& sudo apt install -y wget  gcc cron curl libxmlsec1-dev libxml2-dev libncursesw5-dev libreadline-dev libsqlite3-dev libpq-dev libmysqlclient-dev libssl-dev liblzma-dev libbz2-dev libffi-dev build-essential python-is-python3 nodejs npm python3-dev python3-pip python3-setuptools zlib1g-dev tk-dev xz-utils
 && sudo mkdir -p -m 755 /etc/apt/keyrings \
 && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
 && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
